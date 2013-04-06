467940b33e6fa1123e1f1ccf7d50397a897e6e09
David Idol <david.idol@gmail.com>
2013-04-06 17:52:48 -0400

    Squashed commit of the following:
    commit 862022329a70e9bbcdc454435fe8206a53dd0518
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 17:47:29 2013 -0400
    
        Minor changes
    
    commit 2151a3756c62aa8e3801610bea2d14b3a61e4440
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 17:20:02 2013 -0400
    
        Fixed Facebook authentication
    
    commit 033aac51a70e0318afc0f56467be2344eb04a01a
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 16:55:15 2013 -0400
    
        Changes to attempt login
    
    commit 663b0243137ddb4623ef95afbb26fcd0de179b23
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 15:25:27 2013 -0400
    
        Added logic to server.js
    
    commit 562c0165f4b64b264f99b7fb86a76c69f26a24da
    Merge: cd73bd6 629297f
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 15:25:02 2013 -0400
    
        Merge branch 'master' of https://github.com/idolize/musicapp into facebook
    
    commit cd73bd686ec13caf9645316e7a2bd152324d8f21
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 15:22:15 2013 -0400
    
        First commit for Facebook integration
    
    commit e1c73e23d2803f2f9274531b54dd0ef2284a06e4
    Author: David Idol <david.idol@gmail.com>
    Date:   Sat Apr 6 14:48:13 2013 -0400
    
        New branch
    
        Created new branch

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+var title = 'Music App';
+
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'CollaboPlay' });
+  res.render('index', { title: 'CollaboPlay', user: req.user });
+};
+
+
+/*
+ * GET account page.
+ */
+
+exports.account = function(req, res){
+	res.render('account', { title: title, user: req.user });
 };
